c83c01fce9449019a3dedf428e7b7b4e2f84eb72
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-08-10 18:35:20 +0200

    feat: set up Alembic, create and run migration

--- db/database.py
+++ db/database.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./library_app.db"
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
 class Base(DeclarativeBase):

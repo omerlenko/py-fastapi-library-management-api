fc880b0c0cb101b57572a0edeec5f88c34ab2f45
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-08-10 17:54:49 +0200

    feat: add Author and Book models in models.py

---
+++ db/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from database import Base
+from sqlalchemy import String, Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+class Author(Base):
+    __tablename__ = "authors"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True)
+    bio: Mapped[str] = mapped_column(Text)
+
+    books: Mapped[list["Book"]] = relationship(back_populates="author", cascade="all, delete-orphan")
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(255))
+    summary: Mapped[str] = mapped_column(String(511))
+    publication_date: Mapped[datetime] = mapped_column()
+    author_id: Mapped[int] = mapped_column(ForeignKey("authors.id", ondelete="CASCADE"))
+
+    author: Mapped["Author"] = relationship(back_populates="books")

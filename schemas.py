bffc7ad39f63a0af1dbde19a92eba5390fe8dacc
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-08-11 18:07:15 +0200

    feat: add Author and Book schemas

---
+++ schemas.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Annotated
+
+
+class AuthorBase(BaseModel):
+    name: Annotated[str, Field(max_length=100)]
+    bio: str
+
+
+class AuthorCreate(AuthorBase):
+    pass
+
+
+class AuthorRead(AuthorBase):
+    id: int
+    model_config = ConfigDict(from_attributes=True)
+
+
+class BookBase(BaseModel):
+    title: Annotated[str, Field(max_length=255)]
+    summary: Annotated[str, Field(max_length=511)]
+    publication_date: datetime
+
+
+class BookCreate(BookBase):
+    author_id: int
+
+
+class BookRead(BookBase):
+    id: int
+    author: AuthorRead
+    model_config = ConfigDict(from_attributes=True)
